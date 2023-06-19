59dbd09925ab4daac421086daf66393d036a4c65
Huw Rhys Jones <43721202+dubway420@users.noreply.github.com>
2023-06-19 23:00:36 +0100

    Create example.py
    Creating a homework

---
+++ example.py
@@ -0,0 +1,46 @@
+# This is called a comment. Any line in Python that starts with a hash is a comment line. 
+# This is simply a statement intended to be read by humans only. It will be completely ignored
+# by the programme itself. Comment lines are important to explain to other humans (including
+# your future self) what you have done and why. It's like an instruction manual.
+
+# ============================= PART 1 ============================= 
+
+#Below we are defining a variable. The term on the left is the name of the variable (words). 
+# This variable is of the string type - strings are just words inside inverted commas.
+words = "I love beer!"
+
+# Below is an alternative definition of the variable on line 10. However, in this case it is commented out. 
+# words = "I hate beer!"
+
+# Below is a print statement. Everything inside the curved brackets is written out to the terminal.
+# In this case, we are asking it to print the variable we defined above.
+Print(words)
+
+# Try pressing the play button (⏵) in the top right hand corner to run this code. What do you see in the terminal below?
+
+# ============================= PART 2 =============================
+
+# Go back to line 10 and comment it out - place a hash at the start of the line. It will now be ignored by Python when run.
+# You can also toggle the commenting or uncommenting of a line by moving the cursor to that line (for example by clicking 
+# on it and then pressing Ctrl+/
+
+# Now go to line 13 and uncomment it out - remove the hash at the start of the line. Run the code again by pressing the
+# play buttom. What do you see this time?
+
+# ============================= PART 3 =============================
+
+# Next we will define a new string variable below. You just have to give it a name (it was called words above), then an 
+# equals symbol, then some text within inverted commas. It can be called anything you want and the text can be anything
+# you want. Define this new variable on line 36.
+
+
+
+# Next, we will print out the variable we defined on line 36. Write a print statement below, similar to that on line 17.
+# It should be pretty much the same, but instead of words, it'll be the name of the variable defined on line 36.
+# Write this print statement on line 42.
+
+
+
+# When you've done all that, press the run button again. What happens now?
+
+
