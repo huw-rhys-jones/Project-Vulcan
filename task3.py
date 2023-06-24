9fbe54932cbf0e325f867faefb83116ee8ab9948
Huw Rhys Jones <43721202+dubway420@users.noreply.github.com>
2023-06-24 23:28:20 +0000

    Homework 6

---
+++ task3.py
@@ -0,0 +1,36 @@
+# This homework assignment is about lists
+
+print(" ")
+
+# ================ Definition ====================
+
+# Let's define the list from the notes
+list_first = [0, "one",  3.0]
+
+# Let's take a look at it
+print(list_first)
+
+print(" ")
+
+# ================ Definition ====================
+
+# Let's look at individual elements of our list. 
+# Remember from the issue description that the 
+# indexing system for Python starts at zero
+
+print("The zeroth element of the list is", list_first[0])
+
+# As you can see, you access the n-th element of a list by putting the index in 
+# square ([]) brackets. Above we are requesting element zero (0).
+
+# Similarly, we can access the next element as such: 
+
+print("Element one in the list is", list_first[1])
+
+
+# Task3:
+# Your task is to access element [3] by writing a print statement on the line below.
+
+
+
+# When you have attempted this, go back to the issue notes
---
+++ task4.py
@@ -0,0 +1,41 @@
+# Task 4.0 is to define a list with four elements
+# I don't mind what values you give the elements in this
+# list, as long as there are four of them i.e one more 
+# than list_first had. Call this list list_second
+
+
+
+# Task 4.1: write a print statement to access element [3]
+# of list_second. If it has four elements, this shouldn't
+# give you an index out of range error like you had before
+
+
+
+# Task 4.2 is to access element [-1] of list_second.
+# Compare this output to the output of the command from the 
+# task 4.1. What do you notice?
+
+
+
+# ============= Methods ===================================
+
+# I mentioned in the homework notes that some variable types,
+# such as lists, have built-in functions called methods. 
+# You can find a full list of methods associated with lists 
+# here: https://docs.python.org/3/tutorial/datastructures.html
+# If you have free time after this homework, I encourage you 
+# to read through the above linked website and try out one or 
+# two other methods associated with lists. Some of the wording
+# and notation there may seem confusing, but always the best 
+# way to learn is just to mess around and try stuff out. The
+# worst you'll get is an error and you and just undo and try 
+# again.
+
+# Task 4.3: we will use the append method to append the integer
+# 67 to the list. Now print element [-1] of this list. What is it?
+
+
+
+# That's it! Please go back to the notes in the issue and read the
+# final part.
+
